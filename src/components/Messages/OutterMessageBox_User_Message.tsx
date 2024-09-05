9ba0b6043e67f8d31f238b51c139c0648dd3e3a0
Shreyash <shreyas.techoceanic@gmail.com>
2024-09-05 11:16:22 +0530

    random

---
+++ src/components/Messages/OutterMessageBox_User_Message.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const OutterMessageBox_User_Message = () => {
+  return (
+    <View>
+      <Text>OutterMessageBox_User_Message</Text>
+    </View>
+  );
+};
+
+export default OutterMessageBox_User_Message;
+
+const styles = StyleSheet.create({});
